872153b643772e00fce64f811ea8a49fdc651d34
Ahmad Ramli <ahmad.ramli37@gmail.com>
2024-05-06 09:03:49 +0700

    send and upload function for mobile notification

--- src/pages/mobile-notification/components/modalForm.tsx
+++ src/pages/mobile-notification/components/modalForm.tsx
@@ -1,7 +1,17 @@
 import { Api } from '@/api/api'
-import { CheckOutlined } from '@ant-design/icons'
-import { Button, Form, Input, Modal, Space, message, notification } from 'antd'
+import { CheckOutlined, PlusOutlined } from '@ant-design/icons'
+import {
+  Button,
+  Form,
+  Input,
+  Modal,
+  Space,
+  Upload,
+  message,
+  notification,
+} from 'antd'
 import { useEffect, useState } from 'react'
+import type { UploadFile, UploadProps } from 'antd'
 
 interface iProps {
   token: string
@@ -10,6 +20,8 @@ interface iProps {
   handleClose: any
 }
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
 const ModalFormMobileNotification = ({
   token,
   isOpen,
@@ -17,15 +29,34 @@ const ModalFormMobileNotification = ({
   data,
 }: iProps) => {
   const [loading, setLoading] = useState(false)
+  const [image, setImage] = useState<UploadFile[]>([])
   const [form] = Form.useForm()
 
   useEffect(() => {
     if (data) {
       form.setFieldsValue(data)
+
+      if (data.image) {
+        setImageFile(data.image)
+      }
     } else {
       form.resetFields()
+      setImage([])
     }
   }, [isOpen])
+
+  const setImageFile = (image_url: string) => {
+    let files: any = []
+    let itemImage = {
+      uid: Math.floor(1000 + Math.random() * 9000),
+      name: `file-image`,
+      status: 'done',
+      url: image_url,
+    }
+
+    files.push(itemImage)
+    setImage(files)
+  }
 
   const onSubmit = (values: any) => {
     setLoading(true)
@@ -44,6 +75,22 @@ const ModalFormMobileNotification = ({
         message.error(err.data.message)
       })
       .finally(() => setLoading(false))
+  }
+
+  const handleChangeImage: UploadProps['onChange'] = ({ file, fileList }) => {
+    setImage(fileList)
+
+    if (file.status === 'done') {
+      form.setFieldValue('image', file.response.data.file_path)
+
+      // set file url into image variable
+      setImageFile(file.response.data.file_path)
+    }
+  }
+
+  const onRemoveImage: UploadProps['onRemove'] = (file) => {
+    // console.log(file)
+    setImage([])
   }
 
   return (
@@ -66,6 +113,27 @@ const ModalFormMobileNotification = ({
         <Form.Item name={`message`} label="Message">
           <Input.TextArea rows={3} />
         </Form.Item>
+
+        {isOpen && (
+          <Form.Item label="Upload Image" name="image">
+            <Upload
+              action={`${API_URL}/mobile-notifications/upload-image`}
+              headers={{
+                Authorization: `Bearer ${token}`,
+              }}
+              listType="picture-card"
+              onChange={handleChangeImage}
+              maxCount={1}
+              fileList={image}
+              onRemove={onRemoveImage}
+            >
+              <div>
+                <PlusOutlined />
+                <div style={{ marginTop: 8 }}>Upload</div>
+              </div>
+            </Upload>
+          </Form.Item>
+        )}
 
         <Form.Item style={{ marginTop: '35px', marginBottom: '0' }}>
           <Space>
